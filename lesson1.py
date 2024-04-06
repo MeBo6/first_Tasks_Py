e103a5cf5ab6d91feaebf69daf1a657a52d7f0d9
MeBo6 <gmerabishvili1236@gmail.com>
2024-04-06 16:10:09 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,57 @@
+# print(5 + 2)
+# print(5 - 2)
+# print(5 * 2)
+# print(5 / 2)
+# print(5 % 2)
+# print(5 ** 2)
+# print(5 // 2)
+# print(a + 5)
+# b = a + 5
+# print(5 == 2)
+# print(5 != 2)
+# print(5 > 2)
+# print(5 < 2)
+# print(5 >= 2)
+# print(5 <= 2)
+# print(5>2 and 0==0)
+# print(5>2 or 0!=0)
+# print(not(5<2 and 0==0))
+
+# TASKS
+
+# 1
+
+# a = 2024
+
+
+# birthYear = int(input("write birth year: "))
+
+# age = a - birthYear
+
+# print("Your age is", age)
+
+# 2
+
+# a = int(input("write length:" ))
+
+# b = int(input("write width:" ))
+
+# S = a*b
+
+# print(S)
+
+# 3
+
+# C = int(input("write Celsius:" ))
+
+# F = 9/5 * C + 32
+
+# print(F)
+
+# 4
+
+# a = input("write decimal: ")
+# print(int(float(a)))
+
+   
+
