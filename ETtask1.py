a81e66c85bb1459d0726b1f16b734faadb200709
MeBo6 <gmerabishvili1236@gmail.com>
2024-04-13 23:28:26 +0400

    first commit

---
+++ ETtask1.py
@@ -0,0 +1,26 @@
+# TASK1 log in with email and password
+
+# email = 'email@edutech.com'
+# password = 'edutech'
+
+# inputEmail = input('write your email: ')
+# inputPassword = input('write your password: ')
+
+# if email==inputEmail and password==inputPassword:
+#   print('You logged in!')
+# elif email==inputEmail and password!=inputPassword:
+#   print('try again!')
+# elif email!=inputEmail and password==inputPassword:
+#   print('try again!')
+# else:
+#   print('tinki vinki')
+
+
+# TASK2 check if password has more than 8 chars
+
+# password = '12345678'
+
+# if len(password) >= 8:
+#   print('correct')
+# else:
+#   print('password should contain at least 8 characters')
