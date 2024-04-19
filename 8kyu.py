8cbe7f7395d2b8c83eda3f2da13ae9abf2941e2b
MeBo6 <gmerabishvili1236@gmail.com>
2024-04-19 23:45:12 +0400

    first commit

---
+++ 8kyu.py
@@ -0,0 +1,37 @@
+# 1: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return 'Even'
+#     else:
+#         return 'Odd'
+
+# 2: This code does not execute properly. Try to figure out why.
+
+# def multiply(a, b):
+#     return a * b 
+
+# # 3: In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+# def make_negative( number ):
+#     if number<=0:
+#          return number
+#     elif number>0:
+#         return -number
+
+# def make_negative( number ):
+#     return -abs(number)
+    
+# 4: You get an array of numbers, return the sum of all of the positives ones.Example [1,-4,7,12] => 1 + 7 + 12 = 20 Note: if there is nothing to sum, the sum is default to 0
+
+# def positive_sum(arr):
+#     positiveSum = 0
+#     for num in arr:
+#         if num>0:
+#             positiveSum += num
+#     return positiveSum
+
+# 5 Complete the solution so that it reverses the string passed into it.
+
+# def bool_to_word(boolean):
+#     return 'Yes' if boolean else 'No'
